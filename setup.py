28e539ef074e70a69d0ede2149783bd40aa376f0
Tucker Burgin <tburgin@umich.edu>
2019-08-29 09:42:15 -0400

    Fix imports in setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
-import setuptools
+import sys
+from setuptools import setup, find_packages
+import versioneer
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -7,16 +9,16 @@ with open("README.md", "r") as fh:
 needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
 pytest_runner = ['pytest-runner'] if needs_pytest else []
 
-setuptools.setup(
+setup(
     name="atesa",
     version="1.0",
     author="Tucker Burgin",
     author_email="tburgin@umich.edu",
-    description="Automates performing and analyzing the results of aimless shooting transition path sampling with Amber.",
+    description="Python program for automating Aimless Transition Ensemble Sampling and Analysis (ATESA) with the Amber molecular simulations package.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/team-mayes/atesa",
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
